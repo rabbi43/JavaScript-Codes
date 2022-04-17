8df95122cc9dbfbad85dd96e7a4ea0597926ee1b
Fazle Rabbi <71178740+fh-rabbi@users.noreply.github.com>
2022-04-17 14:01:37 +0600

    Update 1_xhr.js

--- 7_Api-Calling/1_xhr.js
+++ 7_Api-Calling/1_xhr.js
@@ -1,64 +1,63 @@
-// JavaScript Document
-// 15 April,2022
-// author : fazle rabbi
-// all codes written by quoda code editor (mod/premium) apk using my Android Phone🤩
-// facebook: cutt.ly/frfb
-
-// I added these keywords because i have no computer,i have only an android deivice,i use quoda code editor (premium) apk for writing codes.
-// these keywords help me on code auto completion
-/*
-JavaScript Document
-document,write,getElementById,querySelector
-for,var,let,const,add,style,
-eventListener,classList,parentElement
-children,nextElementSiblong
-previousElementSibling
-querySelectorAll,getElementsByTagName
-createElement,createTextNode,appendChild
-removeChild,insertBefore,function
-innerHTML,Math,sqrt,pow,random,floor,ceil
-max,date new Date(),object,array,
-date.get FullYear,Month,Hours,Minutes
-Day,constractor,pop,push,shift,unshift
-splice,slice,sort,typeof,break,continue,while,do
-if,else if,else,switch,case,default,
-Number,parseInt,parseFloat,toString()
-toFixed,toPrecision,length,charAt,
-toUppercase,concat,inside,Literals,name,Names,names,age,roll,concat,StudentsInfo,Students,student,info,var,let,const,return,console,log,
-*/
-// Xml http request
-// event:onload(),onerror()
-// property: response,responseText,responseType,responseUrl,status,statusText
-// function: open(),send(),setRequestHeader()
-// methods:get,post,put,patch,delete
-// API: https://jsonplaceholder.typicode.com/posts
-
-function br(){
-   document.write("<br>");
-}
-// =========================
-
-const makeRequest=(method,url,data)=>{
-  return const xhr = XMLHttpRequest();
-   xhr.open(method,url)
-   xhr.setRequestHeader('Content-Type','Application/json')
+const makeRequest = (m,u,d)=>{
+   const xhr = new XMLHttpRequest();
+   xhr.open(m,u)
+   
+   xhr.setRequestHeader('Content-Type','application/json')
+   
    xhr.onload=()=>{
-      console.log(xhr.response)
+      var x = xhr.response;
+      console.log(JSON.parse(x))
    }
-   xhr.onerror=()=>{
-      console.log("Error here!")
-   }
-   xhr.send(JSON.stringIfy(data));
+   xhr.send(JSON.stringify(d))
 }
 
-const getData =()=>{
+
+/*const getData =()=>{
    makeRequest('GET','https://jsonplaceholder.typicode.com/posts')
 }
-
-getData();
-
+// getData()
 
 
+const sendData = ()=>{
+   makeRequest('POST','https://jsonplaceholder.typicode.com/posts',{
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+   })
+   
+   
+}
+sendData()*/
 
+/*const updateData =()=>{
+   makeRequest('PUT','https://jsonplaceholder.typicode.com/posts/1',{
+      id: 1,
+      title: 'im foo',
+      body: 'im bar',
+      userId: 1,
+      
+   })   
+}
+
+updateData()
+
+*/
+/*const updateSingleData =()=>{
+   makeRequest('PATCH','https://jsonplaceholder.typicode.com/posts/1',{
+      
+      title: 'im changed',
+      
+      
+   })   
+}
+
+updateSingleData()
+
+*/
+
+const deleteData =()=>{
+   makeRequest('DELETE','https://jsonplaceholder.typicode.com/posts/1')  
+}
+deleteData()
 
 
